a40690e32c267090064022045619e6f8cada5563
Николай Сердученко <nikserduchenko@yandex.ru>
2025-03-23 01:39:42 +0300

    chore: add mobile check

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,16 @@ import '@fontsource/inter/700.css';
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
+import {Layout} from './components/Layout/Layout.tsx';
 import {Providers} from './components/Providers/Providers.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <Providers>
-            <App />
-        </Providers>
+        <Layout>
+            <Providers>
+                <App />
+            </Providers>
+        </Layout>
     </StrictMode>
 );
