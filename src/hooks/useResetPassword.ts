734e7a431b982fb81f8cd7c91410517129ad8912
Николай Сердученко <nikserduchenko@yandex.ru>
2025-03-24 19:20:01 +0300

    feat: add forgot password page

--- src/hooks/useResetPassword.ts
+++ src/hooks/useResetPassword.ts
@@ -1,8 +1,13 @@
 import {useMutation} from '@tanstack/react-query';
+import {useNavigate} from 'react-router-dom';
 import {SERVICES} from '../api';
 
 export const useResetPassword = () => {
+    const navigate = useNavigate();
     return useMutation({
         mutationFn: SERVICES.AuthService.resetPassword,
+        onSuccess() {
+            navigate('/confirmation');
+        },
     });
 };
--- src/pages/ForgotPassword/ForgotPassword.tsx
+++ src/pages/ForgotPassword/ForgotPassword.tsx
@@ -16,7 +16,6 @@ export const ForgotPassword: FC = () => {
     const toBack = () => {
         navigate(-1);
     };
-
     return (
         <>
             <Helmet>
