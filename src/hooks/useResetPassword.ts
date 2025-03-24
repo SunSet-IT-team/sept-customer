fe09f00b181dd6a0ef74924d38adcb1a793f0637
Николай Сердученко <nikserduchenko@yandex.ru>
2025-03-24 22:04:14 +0300

    chore: remove auto redirect after reset password mutation

--- src/hooks/useResetPassword.ts
+++ src/hooks/useResetPassword.ts
@@ -1,13 +1,8 @@
 import {useMutation} from '@tanstack/react-query';
-import {useNavigate} from 'react-router-dom';
 import {SERVICES} from '../api';
 
 export const useResetPassword = () => {
-    const navigate = useNavigate();
     return useMutation({
         mutationFn: SERVICES.AuthService.resetPassword,
-        onSuccess() {
-            navigate('/confirmation');
-        },
     });
 };
