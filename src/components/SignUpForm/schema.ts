c3bbade541e57a7d1ef4218e1792b0fc7a34b09a
Николай Сердученко <nikserduchenko@yandex.ru>
2025-03-24 14:06:49 +0300

    chore: change regex for phone

--- src/components/SignUpForm/schema.ts
+++ src/components/SignUpForm/schema.ts
@@ -1,6 +1,12 @@
 import {z} from 'zod';
 
-const phoneRegex = /(^8|7|\+7)((\d{10})|(\s\(\d{3}\)\s\d{3}\s\d{2}\s\d{2}))/;
+const phoneRegex = /(^8|\+7)((\d{10})|(\s\(\d{3}\)\s\d{3}\s\d{2}\s\d{2}))/;
+const onlyDigitsRegex = /^\+?\d+$/;
+
+const cleanPhoneNumber = (phone: string) => {
+    return phone.replace(/[\(\)\-\s]/g, ''); // Убираем скобки, дефисы и пробелы
+};
+
 export const signUpFormSchema = z.object({
     fullname: z.string().min(1, {message: 'Поле обязательно для заполнения'}),
     email: z
@@ -10,8 +16,13 @@ export const signUpFormSchema = z.object({
     phone: z
         .string()
         .min(1, {message: 'Поле обязательно для заполнения'})
-        .regex(phoneRegex, {
-            message: 'Введите российский номер телефона в формате +79181234567',
+        .transform(cleanPhoneNumber)
+        .refine((phone) => onlyDigitsRegex.test(phone), {
+            // Проверка, что номер состоит только из цифр
+            message: 'Номер телефона должен содержать только цифры',
+        })
+        .refine((phone) => phoneRegex.test(phone), {
+            message: 'Введите корректный российский номер телефона',
         }),
     password: z
         .string()
