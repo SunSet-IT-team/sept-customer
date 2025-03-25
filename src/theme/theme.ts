3b9b0314b1ca36539b1a5231b02cc6a48e6f467e
Николай Сердученко <nikserduchenko@yandex.ru>
2025-03-25 23:27:36 +0300

    feat: add navbar and layout

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -9,11 +9,21 @@ export const theme: ThemeOptions = createTheme({
         secondary: {
             main: '#4D4D4D',
         },
+        background: {
+            default: '#4D4D4D',
+            paper: 'white',
+        },
+        text: {
+            primary: 'white',
+        },
     },
     typography: {
         fontFamily: 'Inter, Arial, sans-serif',
         h1: {
             fontSize: '48px',
+        },
+        body2: {
+            fontSize: '13px',
         },
     },
     shape: {
@@ -22,7 +32,33 @@ export const theme: ThemeOptions = createTheme({
     components: {
         MuiButton: {
             defaultProps: {
-                disableElevation: true, //Отключаем тень
+                disableElevation: true,
+            },
+        },
+        MuiBottomNavigation: {
+            styleOverrides: {
+                root: {
+                    backgroundColor: '#4D4D4D',
+                    borderRadius: '40px',
+                    minHeight: '71px',
+                    position: 'relative',
+                },
+            },
+        },
+        MuiBottomNavigationAction: {
+            styleOverrides: {
+                root: {
+                    color: 'white',
+                    justifyContent: 'end',
+                    paddingBottom: '10px',
+                    borderRadius: '40px',
+                    '& .MuiBottomNavigationAction-label': {
+                        fontSize: '13px',
+                    },
+                    '&.Mui-selected .MuiBottomNavigationAction-label': {
+                        fontSize: '13px',
+                    },
+                },
             },
         },
     },
