1d896adb6baf575eee4d73f573ac020549fb6fd7
Николай Сердученко <nikserduchenko@yandex.ru>
2025-03-25 23:27:36 +0300

    feat: add navbar and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import {Route, Routes} from 'react-router-dom';
 import './App.css';
+import Layout from './components/Layout/Layout';
 import {Auth} from './pages/Auth/Auth';
 import {Confirmation} from './pages/Confirmation/Confirmation';
 import {ForgotPassword} from './pages/ForgotPassword/ForgotPassword';
+import {Home} from './pages/Home/Home';
 import {SignIn} from './pages/SignIn/SignIn';
 import {SignUp} from './pages/SignUp/SignUp';
 function App() {
     return (
         <Routes>
+            <Route element={<Layout />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/my-calls" element={<>My calls</>} />
+                <Route path="/profile" element={<>Profile</>} />
+            </Route>
             <Route path="/auth" element={<Auth />} />
             <Route path="/sign-up" element={<SignUp />} />
             <Route path="/sign-in" element={<SignIn />} />
---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,26 @@
+import {Box, Paper} from '@mui/material';
+import {FC} from 'react';
+import {Outlet} from 'react-router-dom';
+import {Navbar} from '../Navbar/Navbar';
+
+const Layout: FC = () => {
+    return (
+        <Box px={'20px'} position={'relative'}>
+            <Outlet />
+            <Paper
+                sx={{
+                    position: 'fixed',
+                    bottom: '20px',
+                    left: '20px',
+                    right: '20px',
+                    borderRadius: '30px',
+                }}
+                elevation={0}
+            >
+                <Navbar />
+            </Paper>
+        </Box>
+    );
+};
+
+export default Layout;
