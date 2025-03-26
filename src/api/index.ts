6be6ecfbef8f96d800b02433b3b58c923de1a0e6
Николай Сердученко <nikserduchenko@yandex.ru>
2025-03-26 22:49:18 +0300

    chore: add api foundation for services list

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,8 @@
 export * as API_ROUTES from './routes';
 
 import {AuthService} from './services/auth/auth.service';
+import {ServicesService} from './services/services/services.service';
 export const SERVICES = {
     AuthService,
+    ServicesService,
 };
