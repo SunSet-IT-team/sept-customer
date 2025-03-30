15d6a2e81605578e05ab76d72382fce483cdf931
Николай Сердученко <nikserduchenko@yandex.ru>
2025-03-31 00:00:45 +0300

    chore: add chat component

--- src/pages/OrderChat/OrderChat.tsx
+++ src/pages/OrderChat/OrderChat.tsx
@@ -1,5 +1,22 @@
+import {Stack, Typography} from '@mui/material';
 import {FC} from 'react';
+import {Chat} from '../../components/Chat/Chat';
 
 export const OrderChat: FC = () => {
-    return <div></div>;
+    return (
+        <Chat
+            interlocutor={{
+                name: 'ООО Септики',
+                imgUrl: '',
+            }}
+            componentAboveChat={
+                <Stack justifyContent={'center'} alignItems={'center'}>
+                    <Typography fontSize={'14px'}>Заявка №4506</Typography>
+                    <Typography fontSize={'14px'}>
+                        Услуга: сборка септика
+                    </Typography>
+                </Stack>
+            }
+        />
+    );
 };
