16b0ab6537dc21ddbf660b61ac93d29718dd8160
Николай Сердученко <nikserduchenko@yandex.ru>
2025-04-01 21:10:48 +0300

    refactor: ForgotPassword form

---
+++ src/components/ForgotPassword/ForgotPasswordContent/styles.ts
@@ -0,0 +1,9 @@
+import {Theme} from '@emotion/react';
+import {SxProps} from '@mui/material';
+
+export const formContainerStyles: SxProps<Theme> = {
+    alignItems: 'center',
+    mt: 3,
+    width: '100%',
+    gap: '10px',
+};
---
+++ src/components/ForgotPassword/styles.ts
@@ -0,0 +1,7 @@
+import {SxProps, Theme} from '@mui/material';
+
+export const submitButton: SxProps<Theme> = {
+    mt: '44px',
+    width: '100%',
+    py: '12px',
+};
