cca72e6b6a9fa6e483ca48f3c7bc580f71b04a6f
Николай Сердученко <nikserduchenko@yandex.ru>
2025-04-01 21:03:35 +0300

    fix: state update and add JSDoc

--- src/store/reset-password/reset-password.slice.ts
+++ src/store/reset-password/reset-password.slice.ts
@@ -2,22 +2,23 @@ import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 import {IInitialStateResetPassword} from './state.interface';
 
 const initialState: IInitialStateResetPassword = {
-    data: {
-        email: '',
-        new_password: '',
-        userId: null,
-    },
+    email: '',
+    new_password: '',
+    userId: null,
 };
 
 export const resetPasswordSlice = createSlice({
     name: 'reset password',
     initialState,
     reducers: {
+        /**
+         * Полностью обновляет данные в слайсе по забытому паролю.
+         */
         setResetPasswordData(
             state,
-            action: PayloadAction<IInitialStateResetPassword['data']>
+            action: PayloadAction<IInitialStateResetPassword>
         ) {
-            state.data = action.payload;
+            return {...state, ...action.payload};
         },
     },
 });
