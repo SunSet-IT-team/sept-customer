5d0eb28c1bfdd491b158d0dc68542785980a34f9
pan0mera <artess.ponomarev@mail.ru>
2025-04-04 00:34:30 +0300

    chore: serviceitems style

--- src/components/ServicesList/ServiceItem/ServiceItem.tsx
+++ src/components/ServicesList/ServiceItem/ServiceItem.tsx
@@ -2,24 +2,14 @@ import {Box, Typography} from '@mui/material';
 import {FC} from 'react';
 import {Link} from 'react-router-dom';
 import {IService} from '../../../types/service';
+import {serviceItemStyle} from './styles';
 
 export const ServiceItem: FC<IService> = ({id, title}) => {
     return (
         <Box
             component={Link}
             to={`service/${id}/new_order`}
-            width={'100%'}
-            display={'flex'}
-            justifyContent={'center'}
-            textAlign={'center'}
-            borderRadius={'15px'}
-            padding={'10px'}
-            color={'black'}
-            sx={{
-                backgroundColor: 'primary.main',
-                aspectRatio: '1/1',
-                textDecoration: 'none',
-            }}
+            sx={serviceItemStyle}
         >
             <Typography variant="h6" fontSize={'18px'}>
                 {title}
---
+++ src/components/ServicesList/ServiceItem/styles.ts
@@ -0,0 +1,15 @@
+import {SxProps, Theme} from '@mui/material';
+
+export const serviceItemStyle: SxProps<Theme> = {
+    mt: '44px',
+    width: '100%',
+    p: '10px',
+    display: 'flex',
+    textAlign: 'center',
+    justifyContent: 'center',
+    borderRadius: '15px',
+    color: 'black',
+    backgroundColor: 'primary.main',
+    aspectRatio: '1/1',
+    textDecoration: 'none',
+};
