df97f1786a35914dd6d22cd60afdde548518274b
pan0mera <artess.ponomarev@mail.ru>
2025-04-04 01:16:28 +0300

    feat: add no find text

--- src/components/ServicesList/ServiceItem/styles.ts
+++ src/components/ServicesList/ServiceItem/styles.ts
@@ -1,7 +1,6 @@
 import {SxProps, Theme} from '@mui/material';
 
 export const serviceItemStyle: SxProps<Theme> = {
-    mt: '44px',
     width: '100%',
     p: '10px',
     display: 'flex',
@@ -9,6 +8,8 @@ export const serviceItemStyle: SxProps<Theme> = {
     justifyContent: 'center',
     borderRadius: '15px',
     color: 'black',
+    fontWeight: 500,
+    alignItems: 'center',
     backgroundColor: 'primary.main',
     aspectRatio: '1/1',
     textDecoration: 'none',
