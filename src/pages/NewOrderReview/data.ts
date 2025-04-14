c3b385db0ee03d34f45283af08ee5a37b76dee18
At1set <iliya9874329@gmail.com>
2025-04-14 16:43:37 +0300

    Fix: NewOrderReview добавил тип данных data.ts
    order_data типизировал IOrderShortInfo

--- src/pages/NewOrderReview/data.ts
+++ src/pages/NewOrderReview/data.ts
@@ -1,6 +1,8 @@
-export const order_data = {
+import { IOrderShortInfo, OrderStatus } from "../../types/order";
+
+export const order_data: IOrderShortInfo = {
     date: '23.05.2024',
     type: 'чистка септика',
-    status: 'выполнен',
+    status: OrderStatus.COMPLETED,
     executor: 'ООО Септик',
 };
