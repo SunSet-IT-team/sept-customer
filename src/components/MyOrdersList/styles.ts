303d7e7c170787319cbfcaa988e21e446827badc
pan0mera <artess.ponomarev@mail.ru>
2025-04-16 13:45:09 +0300

    feat: add no orders text

---
+++ src/components/MyOrdersList/styles.ts
@@ -0,0 +1,13 @@
+import {StylesDictionary} from '../../types/share';
+
+export const useStyles = (): StylesDictionary => {
+    return {
+        list: {
+            mt: '25px',
+        },
+        noFind: {
+            textAlign: 'center',
+            mt: '40px',
+        },
+    };
+};
