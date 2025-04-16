a44cc28f9d91dbe734f26ceaaaf25f1a2c9bbc3f
pan0mera <artess.ponomarev@mail.ru>
2025-04-16 13:45:04 +0300

    feat: add no orders text

--- src/components/MyOrdersList/MyOrdersList.tsx
+++ src/components/MyOrdersList/MyOrdersList.tsx
@@ -1,16 +1,29 @@
-import {Box, Stack} from '@mui/material';
+import {Box, Stack, Typography} from '@mui/material';
 import {FC, RefCallback} from 'react';
 import {OrderCard} from './OrderCard/OrderCard';
 import {IOrder} from '../../types/order';
+import {useStyles} from './styles';
 
 interface IProps {
     orders: IOrder[];
     observedRef: RefCallback<HTMLDivElement>;
 }
 
+/**
+ * Список заказов
+ */
 export const MyOrdersList: FC<IProps> = ({orders, observedRef}) => {
+    const styles = useStyles();
+
+    if (!orders.length)
+        return (
+            <Typography variant="h4" sx={styles.noFind}>
+                У Вас пока нет заказов
+            </Typography>
+        );
+
     return (
-        <Stack spacing={'25px'} mt={'25px'}>
+        <Stack spacing={'25px'} sx={styles.list}>
             {orders.map((el) => {
                 return (
                     <Box key={el.id}>
