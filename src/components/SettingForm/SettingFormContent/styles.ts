43af432040fa88bdc16ecace1aae3295c424b55e
pan0mera <artess.ponomarev@mail.ru>
2025-04-22 00:40:22 +0300

    fix: bug imports

--- src/components/SettingForm/SettingFormContent/styles.ts
+++ src/components/SettingForm/SettingFormContent/styles.ts
@@ -1,4 +1,4 @@
-import {StylesDictionary} from '../../../shared/types/share';
+import {StylesDictionary} from '../../../types/share';
 
 export const useStyles = (): StylesDictionary => {
     return {
